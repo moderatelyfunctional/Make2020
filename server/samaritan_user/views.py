9deca9d9a4ccf28ec4d1fceb8cff9c05b8673654
Jing Lin <moderatelyfunctionalprogrammer@gmail>
2020-02-23 09:06:22 -0500

    webcam

--- server/samaritan_user/views.py
+++ server/samaritan_user/views.py
@@ -1,23 +1,39 @@
 import json
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, StreamingHttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
-from samaritan_user.models import SamaritanState
+from samaritan_user.models import SamaritanState, SamaritanKeyDown
+from samaritan_user.camera import CustomPiCam
+
+cam = None
 
 # Create your views here.
 def index(request):
+	if cam = None:
+		cam = CustomPiCam()
+
 	context = dict()
-	# first = SamaritanState.objects.create(driver='default', state='nr')
-	# first.save()
 	state = SamaritanState.objects.all().first()
 	return render(request, 'index.html', context)
 
 @csrf_exempt
 def drive(request):
 	data = json.loads(request.body)
-	key = data['key']
-	print(key)
+	key_down = SamaritanKeyDown.objects.all().first()
+
 	return HttpResponse(json.dumps({'response': 'Got it'}), status=200)
 
+def gen(camera):
+	while True:
+		frame = cam.get_frame()
+		yield(b'--frame\r\n'
+              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@gzip.gzip_page
+def feed(request):
+	try:
+		return StreamingHttpResponse(gen(PiCam()), content_type='multipart/x-mixed-replace;boundary=frame')
+	except:
+		pass
--- server/server/urls.py
+++ server/server/urls.py
@@ -13,13 +13,37 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import threading
+
 from django.contrib import admin
 from django.urls import path
 
 import samaritan_user.views
+from samaritan_user.models import SamaritanState, SamaritanKeyDown
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', samaritan_user.views.index),
     path('send_key/', samaritan_user.views.drive)
 ]
+
+state_exists = SamaritanState.objects.exists()
+if not state_exists:
+    first_state = SamaritanState.objects.create(driver='default', state='nr')
+    first_state.save()
+
+key_exists = SamaritanKeyDown.objects.exists()
+if not key_exists:
+    first_key = SamaritanKeyDown.objects.create(left=False, right=False, forward=False, backward=False)
+    first_key.save()
+
+def fetch_keys():
+    threading.Timer(5.0, fetch_keys).start()
+    first_key = SamaritanKeyDown.objects.all().first()
+    # print('Key is left={} right={} forward={} backward={}'.format(
+    #     first_key.left, 
+    #     first_key.right, 
+    #     first_key.forward, 
+    #     first_key.backward))
+
+# fetch_keys()
