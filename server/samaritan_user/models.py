632f46afd987efcb822e371945093f6d9f2b4c96
Jing Lin <moderatelyfunctionalprogrammer@gmail>
2020-02-22 22:00:53 -0500

    works

--- server/samaritan_user/models.py
+++ server/samaritan_user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+STATE_CHOICES = (
+	('nr', 'no-request'),
+	('wd', 'waiting-for-driver'),
+	('cd', 'currently-driving')
+)
+class SamaritanState(models.Model):
+	driver = models.CharField('driver', max_length=20, unique=True)
+	state = models.CharField('nr', max_length=2, choices=STATE_CHOICES)
